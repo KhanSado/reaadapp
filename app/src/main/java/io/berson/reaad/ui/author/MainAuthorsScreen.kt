e7b9dd495412273387cd3a2676fcb3802b792f78
Guilherme Berson <bersonintech@gmail.com>
2024-03-24 12:10:25 -0300

    feat(author): list view author

--- app/src/main/java/io/berson/reaad/ui/author/MainAuthorsScreen.kt
+++ app/src/main/java/io/berson/reaad/ui/author/MainAuthorsScreen.kt
@@ -1,35 +1,31 @@
 package io.berson.reaad.ui.author
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import io.berson.reaad.ui.components.GradientSurface
-import io.berson.reaad.ui.utils.mountAuthorList
 import io.berson.reaad.ui.viewmodel.AuthorViewModel
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import io.berson.reaad.ui.components.AppBottomBar
 import io.berson.reaad.ui.components.FloatingActionButton
+import io.berson.reaad.ui.models.Author
 import io.berson.reaad.ui.navigation.DestinationScreen
-import io.berson.reaad.ui.theme.PrimaryColor
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -48,56 +44,45 @@ fun MainAuthorsScreen(navController: NavController, vm: AuthorViewModel) {
         }
     ) {
         GradientSurface {
-            LazyColumn(
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .wrapContentHeight()
-                    .padding(bottom = 90.dp, top = 45.dp),
-                verticalArrangement = Arrangement.Top
-            ) {
-                items(items = mountAuthorList(authorUiState.authorList), itemContent = { item ->
-                    AuthorView(
-                        firstname = item.firstname,
-                        lastname = item.lastname,
-                        clickOnItem = {
-                            navController.navigate("${DestinationScreen.AuthorDetailScreen.name}/${item.documentId}")
-                        }
-                    )
-                    Divider(
-                        color = PrimaryColor,
-                        modifier = Modifier
-                            .padding(start = 24.dp, end = 24.dp)
-                    )
-                })
-            }
+            authorUiState.authorList?.let { it1 -> LazyGridList(
+                authors = it1,
+                navController = navController
+            ) }
         }
     }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
-fun AuthorView(
-    firstname: String = "Jhon",
-    lastname: String = "Piper",
-    clickOnItem: () -> Unit = {}
-){
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(80.dp)
-            .padding(start = 24.dp, top = 24.dp, bottom = 24.dp, end = 24.dp),
-        color = Color.Transparent,
-        onClick = clickOnItem
+fun LazyGridList(
+    authors: List<Author>,
+    navController: NavController
     ) {
-        Column {
-            Text(
-                text = "$firstname $lastname",
-                color = Color.Black,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.SemiBold
-            )
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .width(48.dp)
+            .height(48.dp)
+    ) {
+        items(items = authors) { author ->
+            Box(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .background(Color.White, shape = RoundedCornerShape(8.dp))
+                    .height(48.dp)
+                    .width(48.dp)
+                    .clickable(
+                        onClick = {
+                            navController.navigate("${DestinationScreen.AuthorDetailScreen.name}/${author.documentId}")
+                        }
+                    ),
+
+            ) {
+                Text(
+                    text = "${author.firstname} ${author.lastname}",
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
         }
     }
 }
