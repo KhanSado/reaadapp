e10f63b3f159583b2ac0e9fc522e1f8f23d3da35
Guilherme Berson <bersonintech@gmail.com>
2024-04-18 15:56:48 -0300

    feat(version):update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.berson.reaad"
         minSdk = 27
         targetSdk = 34
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 111
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
