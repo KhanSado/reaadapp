a915e1477736f5108229912ad12d07eddc6aa64e
Guilherme Berson <bersonintech@gmail.com>
2024-04-24 10:04:45 -0300

    feat(release): release version 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.berson.reaad"
         minSdk = 27
         targetSdk = 34
-        versionCode = 114
-        versionName = "1.1.4"
+        versionCode = 120
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
